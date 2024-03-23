835fbcc2ddb84c44b252a0fbbe3970da6b98f1c3
x1a0xf <yyzw@live.com>
2024-03-23 20:49:22 +0800

    feat: Comment with "action" command

--- cmd/actions.go
+++ cmd/actions.go
@@ -2,36 +2,31 @@ package cmd
 
 import (
 	"fmt"
-	"io"
 	"log"
-	"net/http"
 	"time"
 
 	"github.com/91go/gh-alfredworkflow/utils"
-	"github.com/91go/gh-alfredworkflow/utils/secret"
-	aw "github.com/deanishe/awgo"
 	"github.com/google/go-github/v56/github"
-	"github.com/spf13/cobra"
 )
 
 // actionsCmd represents the actions command
-var actionsCmd = &cobra.Command{
-	Use:     "actions",
-	Short:   "Common Operations",
-	Example: "icons/settings.png",
-	Run: func(cmd *cobra.Command, args []string) {
-		actions := []Metadata{
-			{item: "actions token", subtitle: "Enter to set github token", icon: &aw.Icon{Value: "icons/actions-token.svg"}},
-			// {item: "actions sync", subtitle: "Enter to flush repositories local database", icon: &aw.Icon{Value: "icons/actions-sync.svg"}},
-			// {item: "actions update", subtitle: "Enter to check workflow's update", icon: &aw.Icon{Value: "icons/actions-update.svg"}},
-			{item: "actions clean", subtitle: "Enter to clear caches", icon: &aw.Icon{Value: "icons/actions-clean.svg"}},
-		}
-		for _, m := range actions {
-			wf.NewItem(m.item).Valid(false).Subtitle(m.subtitle).Icon(m.icon).Autocomplete(m.item).Title(m.item)
-		}
-		wf.SendFeedback()
-	},
-}
+// var actionsCmd = &cobra.Command{
+// 	Use:     "actions",
+// 	Short:   "Common Operations",
+// 	Example: "icons/settings.png",
+// 	Run: func(cmd *cobra.Command, args []string) {
+// 		actions := []Metadata{
+// 			{item: "actions token", subtitle: "Enter to set github token", icon: &aw.Icon{Value: "icons/actions-token.svg"}},
+// 			// {item: "actions sync", subtitle: "Enter to flush repositories local database", icon: &aw.Icon{Value: "icons/actions-sync.svg"}},
+// 			// {item: "actions update", subtitle: "Enter to check workflow's update", icon: &aw.Icon{Value: "icons/actions-update.svg"}},
+// 			{item: "actions clean", subtitle: "Enter to clear caches", icon: &aw.Icon{Value: "icons/actions-clean.svg"}},
+// 		}
+// 		for _, m := range actions {
+// 			wf.NewItem(m.item).Valid(false).Subtitle(m.subtitle).Icon(m.icon).Autocomplete(m.item).Title(m.item)
+// 		}
+// 		wf.SendFeedback()
+// 	},
+// }
 
 // updateCmd represents the update command
 // var updateCmd = &cobra.Command{
@@ -68,89 +63,89 @@ var actionsCmd = &cobra.Command{
 // }
 
 // tokenCmd represents the token command
-var tokenCmd = &cobra.Command{
-	Use:   "token",
-	Short: "A brief description of your command",
-	Args:  cobra.ExactArgs(1),
-	Run: func(cmd *cobra.Command, args []string) {
-		store := secret.NewStore(wf)
-		token := args[0]
-
-		if token, err := store.GetAPIToken(); err != nil || len(token) == 0 {
-			wf.NewWarningItem("No Token Found.", "").Valid(false).Title("No Token Found.")
-			wf.SendFeedback()
-		}
-
-		if err := store.Store(secret.KeyGithubAPIToken, token); err != nil {
-			wf.NewWarningItem("Store Token Failed.", err.Error()).Valid(false).Title("Store Token Failed.")
-			wf.SendFeedback()
-		}
-		wf.NewItem("Set Token Successfully.").Title("Set Token Successfully.")
-		wf.SendFeedback()
-	},
-}
+// var tokenCmd = &cobra.Command{
+// 	Use:   "token",
+// 	Short: "A brief description of your command",
+// 	Args:  cobra.ExactArgs(1),
+// 	Run: func(cmd *cobra.Command, args []string) {
+// 		store := secret.NewStore(wf)
+// 		token := args[0]
+//
+// 		if token, err := store.GetAPIToken(); err != nil || len(token) == 0 {
+// 			wf.NewWarningItem("No Token Found.", "").Valid(false).Title("No Token Found.")
+// 			wf.SendFeedback()
+// 		}
+//
+// 		if err := store.Store(secret.KeyGithubAPIToken, token); err != nil {
+// 			wf.NewWarningItem("Store Token Failed.", err.Error()).Valid(false).Title("Store Token Failed.")
+// 			wf.SendFeedback()
+// 		}
+// 		wf.NewItem("Set Token Successfully.").Title("Set Token Successfully.")
+// 		wf.SendFeedback()
+// 	},
+// }
 
 // pruneCmd represents the logout command
-var pruneCmd = &cobra.Command{
-	Use:   "clean",
-	Short: "CLEAR CACHES",
-	Run: func(cmd *cobra.Command, args []string) {
-		// 删除 my.json
-		// if err := wf.ClearCache(); err != nil {
-		// 	wf.NewWarningItem("Clear Caches Failed.", err.Error()).Valid(false).Title("Clear Caches Failed.")
-		// 	wf.SendFeedback()
-		// }
-
-		if err := wf.Config.Set("username", utils.NewGithubClient(token).GetUsername(), false).Do(); err != nil {
-			wf.NewWarningItem("Clear Caches Failed.", err.Error()).Valid(false).Title("Clear Caches Failed.")
-			wf.SendFeedback()
-		}
-		wf.NewItem("Clear Caches Successfully.").Title("Clear Caches Successfully.").Valid(false)
-		wf.SendFeedback()
-	},
-}
+// var pruneCmd = &cobra.Command{
+// 	Use:   "clean",
+// 	Short: "CLEAR CACHES",
+// 	Run: func(cmd *cobra.Command, args []string) {
+// 		// 删除 my.json
+// 		// if err := wf.ClearCache(); err != nil {
+// 		// 	wf.NewWarningItem("Clear Caches Failed.", err.Error()).Valid(false).Title("Clear Caches Failed.")
+// 		// 	wf.SendFeedback()
+// 		// }
+//
+// 		if err := wf.Config.Set("username", utils.NewGithubClient(token).GetUsername(), false).Do(); err != nil {
+// 			wf.NewWarningItem("Clear Caches Failed.", err.Error()).Valid(false).Title("Clear Caches Failed.")
+// 			wf.SendFeedback()
+// 		}
+// 		wf.NewItem("Clear Caches Successfully.").Title("Clear Caches Successfully.").Valid(false)
+// 		wf.SendFeedback()
+// 	},
+// }
 
 // syncCmd represents the updateRepos command
-var syncCmd = &cobra.Command{
-	Use:    "sync",
-	Hidden: true,
-	Run: func(cmd *cobra.Command, args []string) {
-		// sync repo
-		if _, err := UpdateRepositories(token); err != nil {
-			wf.NewWarningItem("Sync Failed.", err.Error()).Valid(false).Title("Sync Failed.")
-			wf.SendFeedback()
-		}
-
-		// gh.yml
-		url := wf.Config.GetString("url")
-		if url != "" {
-			resp, err := http.Get(url)
-			if err != nil {
-				return
-			}
-			defer resp.Body.Close()
-
-			data, err := io.ReadAll(resp.Body)
-			if err != nil {
-				return
-			}
-			err = wf.Cache.Store(CustomRepo, data)
-			if err != nil {
-				return
-			}
-		}
-
-		wf.NewItem("Sync Repos Successfully.").Title("Sync Repos Successfully.").Valid(false)
-		wf.SendFeedback()
-	},
-}
+// var syncCmd = &cobra.Command{
+// 	Use:    "sync",
+// 	Hidden: true,
+// 	Run: func(cmd *cobra.Command, args []string) {
+// 		// sync repo
+// 		if _, err := UpdateRepositories(token); err != nil {
+// 			wf.NewWarningItem("Sync Failed.", err.Error()).Valid(false).Title("Sync Failed.")
+// 			wf.SendFeedback()
+// 		}
+//
+// 		// gh.yml
+// 		url := wf.Config.GetString("url")
+// 		if url != "" {
+// 			resp, err := http.Get(url)
+// 			if err != nil {
+// 				return
+// 			}
+// 			defer resp.Body.Close()
+//
+// 			data, err := io.ReadAll(resp.Body)
+// 			if err != nil {
+// 				return
+// 			}
+// 			err = wf.Cache.Store(CustomRepo, data)
+// 			if err != nil {
+// 				return
+// 			}
+// 		}
+//
+// 		wf.NewItem("Sync Repos Successfully.").Title("Sync Repos Successfully.").Valid(false)
+// 		wf.SendFeedback()
+// 	},
+// }
 
 func init() {
-	listCmd.AddCommand(actionsCmd)
-	// actionsCmd.AddCommand(updateCmd)
-	actionsCmd.AddCommand(tokenCmd)
-	actionsCmd.AddCommand(pruneCmd)
-	actionsCmd.AddCommand(syncCmd)
+	// listCmd.AddCommand(actionsCmd)
+	// // actionsCmd.AddCommand(updateCmd)
+	// actionsCmd.AddCommand(tokenCmd)
+	// actionsCmd.AddCommand(pruneCmd)
+	// actionsCmd.AddCommand(syncCmd)
 }
 
 func UpdateRepositories(token string) (int64, error) {
